26af6050e37cf64d3672155202f6f717ccad742d
Meo Huynh <meoh92@gmail.com>
2020-04-07 15:15:15 -0700

    Add navbar to Header component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react"
+// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Header from './components/Header'
 
 class App extends Component {
   render () {
+    const { logged_in, sign_in_path, sign_out_path } = this.props
     return (
       <>
-        <h1>IT WORKED!</h1>
+        <Header logged_in={logged_in} sign_in_path={sign_in_path} sign_out_path={sign_out_path}/>
       </>
     );
   }
