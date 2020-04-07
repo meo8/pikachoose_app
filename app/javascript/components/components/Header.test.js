b1d92cea4e7aa29840197762346d7f9ce6f1b257
Meo Huynh <meoh92@gmail.com>
2020-04-07 16:47:33 -0700

    Add jest and enzyme with one Header component test

---
+++ app/javascript/components/components/Header.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Header from './Header'
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('Header component has Navbar component', () => {
+  const wrap = shallow(<Header/>)
+  expect(wrap.text()).toEqual('<Navbar />')
+})
