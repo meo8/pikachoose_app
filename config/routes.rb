1c02b9ff7adc039a89daf888aa56ff649116d738
Meo Huynh <meoh92@gmail.com>
2020-04-07 15:42:48 -0700

    Quick comment about yield

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # home#index below replaces <%= yield %> in application.html.erb
   get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root to: 'home#index'
 end
