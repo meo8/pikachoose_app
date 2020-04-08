4804011ea9ee055f83de37d0e98622fba5cf56fd
learn <learn@learn-11.attlocal.net>
2020-04-08 10:59:06 -0700

    added mock data for history

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+histories = [
+  {
+    decision: 'the butterfly effect',
+    kind: "movie",
+    link: "https://www.imdb.com/title/tt0289879/ ",
+    is_favorite: true ,
+    comment: "this movie changed my life.",
+    user_id: 1
+  },
+  {
+    decision: 'be kind rewind',
+    kind: "movie",
+    link: "https://www.imdb.com/title/tt0799934/",
+    is_favorite: true ,
+    comment: "this movie is so wholesum & stupid.",
+    user_id: 1
+  },
+  {
+    decision: 'homemade lucky charms cereal',
+    kind: "recipe",
+    link: "https://www.brit.co/homemade-cereal/",
+    is_favorite: false ,
+    comment: "this cereal is trash.",
+    user_id: 1
+  }
+]
+
+histories.each do |attributes|
+  History.create attributes
+end
