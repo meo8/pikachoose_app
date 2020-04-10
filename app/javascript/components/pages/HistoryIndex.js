4dd4979e3d0aaf8b2b40cd4081bf38fdea8da198
Meo Huynh <meoh92@gmail.com>
2020-04-10 09:08:51 -0700

    Format code

--- app/javascript/components/pages/HistoryIndex.js
+++ app/javascript/components/pages/HistoryIndex.js
@@ -1,14 +1,14 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import { ListGroup } from 'reactstrap'; 
 import {Link, Route, BrowserRouter as Router, Switch} from "react-router-dom"
 
-class HistoryIndex extends Component{ 
+class HistoryIndex extends Component{
     render(){
         return(
               <>
                 <div>
                   <div >
-                    <h3>Your Saved Movies</h3>
+                    <h3>Your Saved Films</h3>
                     {this.props.histories.map((history, index) => {
                       return(
                         <ListGroup className="decisionbox" key={ index }>
