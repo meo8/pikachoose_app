bb609f40339debc7ab2be743d1f3838e9c51027b
Meo Huynh <meoh92@gmail.com>
2020-04-11 15:46:21 -0700

    Add styling to HistoryIndex

--- app/javascript/components/pages/HistoryIndex.js
+++ app/javascript/components/pages/HistoryIndex.js
@@ -1,34 +1,27 @@
 import React from "react"
-import { ListGroup } from 'reactstrap'; 
+import { ListGroup } from "reactstrap"
 import { Link } from "react-router-dom"
 
 const HistoryIndex = (props) => {
   return (
     <>
       <div>
-        <h3>Your Bookmarks</h3>
+        <h3 className="history-header">Your Bookmarks</h3>
         {props.histories.map((history, index) => {
           return (
             <ListGroup className="decision-box" key={ index }>
-              <Link to={`/history/${history.id}`} ><h4 id="name"> {history.decision }</h4></Link>
-              <br/>
-              <div>
+              <Link to={`/history/${history.id}`} ><h4 className="film-title"> {history.decision}</h4></Link>
+
+              <div className="film-info">
                 {/* <small>favorited?: {history.is_favorite }  </small> */}
-                <br/>
-                <small>link: <a href={history.link}>{history.link}</a>  </small>
-                <br/>
-                <small>type: {history.kind}  </small>
-                <br/>
-                <small>notes: {history.comment} </small>
+                <p>link: <a href={history.link}>{history.link}</a></p>
+                <p>type: {history.kind}</p>
+                <p>notes: {history.comment}</p>
               </div>
             </ListGroup>
           )
         })}
       </div>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
     </>
   )
 }
