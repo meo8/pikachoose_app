c6b5c3431719d883e82d2c9f13a8713abff9c5d5
Heya Kwon <heyabird@gmail.com>
2020-04-12 16:19:07 -0700

    added more About page content

--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -4,20 +4,22 @@ import { Button } from "reactstrap"
 const About = (props) =>{
   return (
     <div id="aboutpage">
-      <h1>About</h1>
+      {/* <h1>About</h1> */}
       <br/>
       <br/>
-      <h4>About the App </h4>
+      <h2>About the App </h2>
       <br/>
-      <p>What to watch? What to eat?</p>
+      <p>What to watch? What to eat? What to buy?</p>
       <p>Everyday, we have to make so many little decisions.</p>
       <p>Pikachoose is an app that tackles the modern day decision fatigue by letting users quickly arrive at a decision.</p>
       <br/>
       <br/>
-      <h4>About the Team </h4>
+      <h2>About the Team </h2>
       <br/>
       <p>We are four web developers who met at LEARN Academy in San Diego.</p> 
       <p>We love to build humorous and creative solutions to everyday problems and all suffer from the decision fatigue to differing degrees.</p>
+      <br/>
+      <p>*insert member photos linked to our linkedin here*</p>
     </div>
   )
 }
