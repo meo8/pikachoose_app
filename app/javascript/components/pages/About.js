485b2516e722379ad2934c941bb8641d54ff1f8b
learn <learn@learn-11.attlocal.net>
2020-04-13 14:21:34 -0700

    added photos/links

--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -3,6 +3,7 @@ import { Button } from "reactstrap"
 
 const About = (props) =>{
   return (
+    <div>
     <div id="aboutpage">
       {/* <h1>About</h1> */}
       <br/>
@@ -19,8 +20,41 @@ const About = (props) =>{
       <p>We are four web developers who met at LEARN Academy in San Diego.</p> 
       <p>We love to build humorous and creative solutions to everyday problems and all suffer from the decision fatigue to differing degrees.</p>
       <br/>
-      <p>*insert member photos linked to our linkedin here*</p>
     </div>
+   
+    <div class="about_us_imgs">
+          <div class="text">
+            <h3>Elizabeth H.</h3>
+            <h5 ><a href="https://www.linkedin.com/in/elizabeth-h-1649a267/" target="_blank">Linkedin</a>/</h5>    
+            <h5><a href="https://github.com/lizhavird" target="_blank">GitHub</a>/</h5>
+          </div> 
+          <div class="img"><img src={'https://i.imgur.com/7LEjOC2.jpg'}/></div>
+    </div>
+    <div class="about_us_imgs">
+        <div class="text"> 
+          <h3>Meo H.</h3>  
+          <h5><a href="https://www.linkedin.com/in/meoh/" target="_blank">Linkedin</a>/</h5>
+          <h5><a href="https://github.com/meo8/" target="_blank">GitHub</a>/</h5>
+        </div>    
+        <div class="img"><img src={'https://i.imgur.com/fnDeeHX.jpg'}/></div>    
+    </div>
+    <div class="about_us_imgs">
+        <div class="text">    
+          <h3>Heya Kwon</h3>
+          <h5><a href="https://www.linkedin.com/in/heya" target="_blank">Linkedin</a>/</h5>
+          <h5><a href="https://github.com/Heyabird" target="_blank">GitHub</a>/</h5> 
+        </div>
+        <div class="img"><img src={'https://i.imgur.com/LPwvJGh.jpg'}/></div>
+    </div>
+    <div class="about_us_imgs">
+        <div class="text">  
+          <h3>Jonas Fernandes</h3>  
+          <h5><a href="https://www.linkedin.com/in/jonas-fernandes-dev/" target="_blank">Linkedin</a>/</h5>
+          <h5><a href="https://github.com/jonas-fernandes" target="_blank">GitHub</a>/</h5>
+        </div> 
+        <div class="img"><img src={'https://i.imgur.com/F0j1uKL.jpg'}/></div>
+    </div>
+ </div>
   )
 }
 
