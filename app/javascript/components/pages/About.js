28ef8795250687f6e937c7fd2db25a1cb95dd9d9
learn <learn@learn-11.attlocal.net>
2020-04-13 17:55:51 -0700

    added imgs & github/linkedin

--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react"
 import { Button } from "reactstrap"
+import { Image, Container, Col, Row } from 'react-bootstrap';
+import {Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle} from 'reactstrap';
 
 const About = (props) =>{
   return (
-    <div>
+  <>
     <div id="aboutpage">
       {/* <h1>About</h1> */}
       <br/>
@@ -21,40 +24,61 @@ const About = (props) =>{
       <p>We love to build humorous and creative solutions to everyday problems and all suffer from the decision fatigue to differing degrees.</p>
       <br/>
     </div>
-   
-    <div class="about_us_imgs">
-          <div class="text">
-            <h3>Elizabeth H.</h3>
-            <h5 ><a href="https://www.linkedin.com/in/elizabeth-h-1649a267/" target="_blank">Linkedin</a>/</h5>    
-            <h5><a href="https://github.com/lizhavird" target="_blank">GitHub</a>/</h5>
-          </div> 
-          <div class="img"><img src={'https://i.imgur.com/7LEjOC2.jpg'}/></div>
-    </div>
-    <div class="about_us_imgs">
-        <div class="text"> 
-          <h3>Meo H.</h3>  
-          <h5><a href="https://www.linkedin.com/in/meoh/" target="_blank">Linkedin</a>/</h5>
-          <h5><a href="https://github.com/meo8/" target="_blank">GitHub</a>/</h5>
-        </div>    
-        <div class="img"><img src={'https://i.imgur.com/fnDeeHX.jpg'}/></div>    
-    </div>
-    <div class="about_us_imgs">
-        <div class="text">    
-          <h3>Heya Kwon</h3>
-          <h5><a href="https://www.linkedin.com/in/heya" target="_blank">Linkedin</a>/</h5>
-          <h5><a href="https://github.com/Heyabird" target="_blank">GitHub</a>/</h5> 
-        </div>
-        <div class="img"><img src={'https://i.imgur.com/LPwvJGh.jpg'}/></div>
-    </div>
-    <div class="about_us_imgs">
-        <div class="text">  
-          <h3>Jonas Fernandes</h3>  
-          <h5><a href="https://www.linkedin.com/in/jonas-fernandes-dev/" target="_blank">Linkedin</a>/</h5>
-          <h5><a href="https://github.com/jonas-fernandes" target="_blank">GitHub</a>/</h5>
-        </div> 
-        <div class="img"><img src={'https://i.imgur.com/F0j1uKL.jpg'}/></div>
-    </div>
- </div>
+  
+<Container id ="aboutimgs">
+  <Row>
+    <Col xs={4} md={3}>
+      <Card id="card">
+          <CardImg src="https://i.imgur.com/7LEjOC2.jpg"/>
+          <CardBody>
+            <CardTitle>Elizabeth H.</CardTitle>
+            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
+            <CardText>more text if u want</CardText>
+            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/elizabeth-h-1649a267/" target="_blank">Linkedin</a></Button>
+            <Button id="button" color="warning"><a href="https://github.com/lizhavird" target="_blank">GitHub</a></Button>
+          </CardBody>
+        </Card>
+    </Col>    
+    <Col xs={4} md={3}>  
+      <Card id="card">
+          <CardImg src="https://i.imgur.com/fnDeeHX.jpg"/>
+          <CardBody>
+            <CardTitle>Meo H.</CardTitle>
+            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
+            <CardText>more text if u want</CardText>
+            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/meoh/" target="_blank">Linkedin</a></Button>
+            <Button id="button" color="warning"><a href="https://github.com/meo8/" target="_blank">GitHub</a></Button>
+          </CardBody>
+      </Card>
+    </Col>  
+    <Col xs={4} md={3}>
+      <Card id="card">
+          <CardImg src="https://i.imgur.com/LPwvJGh.jpg"/>
+          <CardBody>
+            <CardTitle>Heya Kwon</CardTitle>
+            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
+            <CardText>more text if u want</CardText>
+            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/heya" target="_blank">Linkedin</a></Button>
+            <Button id="button" color="warning"><a href="https://github.com/Heyabird" target="_blank">GitHub</a></Button>
+          </CardBody>
+      </Card>
+    </Col>
+    <Col xs={4} md={3}>
+      <Card id="card">
+          <CardImg src="https://i.imgur.com/F0j1uKL.jpg"/>
+          <CardBody>
+            <CardTitle>Jonas Fernandes</CardTitle>
+            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
+            <CardText>more text if u want</CardText>
+            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/jonas-fernandes-dev/" target="_blank">Linkedin</a></Button>
+            <Button id="button" color="warning"><a href="https://github.com/jonas-fernandes" target="_blank">GitHub</a></Button>
+          </CardBody>
+      </Card>
+    </Col>
+  </Row>
+</Container>
+    
+ </>
   )
 }
 
