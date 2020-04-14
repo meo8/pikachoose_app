5721f89659cd5d9b524a374a98c590d7a7a0d21f
Meo Huynh <meoh92@gmail.com>
2020-04-14 09:36:30 -0700

    Refactor about page

--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -5,80 +5,82 @@ import {Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle} from 'reactstrap';
 
 const About = (props) =>{
+  const teamMembers = [
+    {
+      name: "Elizabeth H.",
+      title: "Full-Stack Web Developer",
+      funFact: "Fun fact",
+      linkedin: "https://www.linkedin.com/in/elizabeth-h-1649a267/",
+      github: "https://github.com/lizhavird",
+      headshot: "https://i.imgur.com/7LEjOC2.jpg"
+    },
+    {
+      name: "Meo Huynh",
+      title: "Full-Stack Web Developer",
+      funFact: "Fun fact",
+      linkedin: "https://www.linkedin.com/in/meoh/",
+      github: "https://github.com/meo8/",
+      headshot: "https://i.imgur.com/fnDeeHX.jpg"
+    },
+    {
+      name: "Heya Kwon",
+      title: "Full-Stack Web Developer",
+      funFact: "Fun fact",
+      linkedin: "https://www.linkedin.com/in/heya",
+      github: "https://github.com/Heyabird",
+      headshot: "https://i.imgur.com/LPwvJGh.jpg"
+    },
+    {
+      name: "Jonas Fernandes",
+      title: "Full-Stack Web Developer",
+      funFact: "Fun fact",
+      linkedin: "https://www.linkedin.com/in/jonas-fernandes-dev/",
+      github: "https://github.com/jonas-fernandes",
+      headshot: "https://i.imgur.com/F0j1uKL.jpg"
+    }
+  ]
+
   return (
-  <>
-    <div id="aboutpage">
-      {/* <h1>About</h1> */}
-      <br/>
-      <br/>
-      <h2>About the App </h2>
-      <br/>
-      <p>What to watch? What to eat? What to buy?</p>
-      <p>Everyday, we have to make so many little decisions.</p>
-      <p>Pikachoose is an app that tackles the modern day decision fatigue by letting users quickly arrive at a decision.</p>
-      <br/>
-      <br/>
-      <h2>About the Team </h2>
-      <br/>
-      <p>We are four web developers who met at LEARN Academy in San Diego.</p> 
-      <p>We love to build humorous and creative solutions to everyday problems and all suffer from the decision fatigue to differing degrees.</p>
-      <br/>
-    </div>
-  
-<Container id ="aboutimgs">
-  <Row>
-    <Col xs={4} md={3}>
-      <Card id="card">
-          <CardImg src="https://i.imgur.com/7LEjOC2.jpg"/>
-          <CardBody>
-            <CardTitle>Elizabeth H.</CardTitle>
-            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
-            <CardText>more text if u want</CardText>
-            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/elizabeth-h-1649a267/" target="_blank">Linkedin</a></Button>
-            <Button id="button" color="warning"><a href="https://github.com/lizhavird" target="_blank">GitHub</a></Button>
-          </CardBody>
-        </Card>
-    </Col>    
-    <Col xs={4} md={3}>  
-      <Card id="card">
-          <CardImg src="https://i.imgur.com/fnDeeHX.jpg"/>
-          <CardBody>
-            <CardTitle>Meo H.</CardTitle>
-            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
-            <CardText>more text if u want</CardText>
-            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/meoh/" target="_blank">Linkedin</a></Button>
-            <Button id="button" color="warning"><a href="https://github.com/meo8/" target="_blank">GitHub</a></Button>
-          </CardBody>
-      </Card>
-    </Col>  
-    <Col xs={4} md={3}>
-      <Card id="card">
-          <CardImg src="https://i.imgur.com/LPwvJGh.jpg"/>
-          <CardBody>
-            <CardTitle>Heya Kwon</CardTitle>
-            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
-            <CardText>more text if u want</CardText>
-            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/heya" target="_blank">Linkedin</a></Button>
-            <Button id="button" color="warning"><a href="https://github.com/Heyabird" target="_blank">GitHub</a></Button>
-          </CardBody>
-      </Card>
-    </Col>
-    <Col xs={4} md={3}>
-      <Card id="card">
-          <CardImg src="https://i.imgur.com/F0j1uKL.jpg"/>
-          <CardBody>
-            <CardTitle>Jonas Fernandes</CardTitle>
-            <CardSubtitle>Full-Stack Web Developer</CardSubtitle>
-            <CardText>more text if u want</CardText>
-            <Button id="button" color="warning"><a href="https://www.linkedin.com/in/jonas-fernandes-dev/" target="_blank">Linkedin</a></Button>
-            <Button id="button" color="warning"><a href="https://github.com/jonas-fernandes" target="_blank">GitHub</a></Button>
-          </CardBody>
-      </Card>
-    </Col>
-  </Row>
-</Container>
-    
- </>
+    <>
+      <div id="aboutpage">
+        {/* <h1>About</h1> */}
+        <br/>
+        <br/>
+        <h2>About the App </h2>
+        <br/>
+        <p>What to watch? What to eat? What to buy?</p>
+        <p>Everyday, we have to make so many little decisions.</p>
+        <p>Pikachoose is an app that tackles the modern day decision fatigue by letting users quickly arrive at a decision.</p>
+        <br/>
+        <br/>
+        <h2>About the Team </h2>
+        <br/>
+        <p>We are four web developers who met at LEARN Academy in San Diego.</p>
+        <p>We love to build humorous and creative solutions to everyday problems and all suffer from the decision fatigue to differing degrees.</p>
+        <br/>
+      </div>
+
+      <Container id="aboutimgs">
+        <Row>
+          {teamMembers.map((member, id) => {
+            return (
+              <Col xs={4} md={3} key={id}>
+                <Card id="card">
+                  <CardImg src={member.headshot}/>
+                  <CardBody>
+                    <CardTitle>{member.name}</CardTitle>
+                    <CardSubtitle>{member.title}</CardSubtitle>
+                    <CardText>{member.funFact}</CardText>
+                    <Button className="linkedin-gihub-btn" color="warning"><a href={member.linkedin} target="_blank">Linkedin</a></Button>
+                    <Button className="linkedin-gihub-btn" color="warning"><a href={member.github} target="_blank">GitHub</a></Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+    </>
   )
 }
 
