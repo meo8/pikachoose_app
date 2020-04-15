6a3ac9352897d0da0cc615e192ac6a8c989254d7
learn <learn@learn-11.attlocal.net>
2020-04-15 13:26:56 -0700

    fixes redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+    #before devise action- store location before user signs in
+    before_action :store_user_location!, if: :storable_location?
+    before_action :authenticate_user!
     skip_before_action :verify_authenticity_token
+    
+    private
+    #gets stored location
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+  
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 end
